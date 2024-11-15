a485f98725e324b784cc096102717be78d92c4ce
Andras Csanyi <andras.csanyi@pm.me>
2024-11-15 18:00:43 +0100

    changes

--- Core/Application/Contracts.cs
+++ Core/Application/Contracts.cs
@@ -2,33 +2,44 @@ namespace EncyclopediaGalactica.Core.Application;
 
 public class ApplicationInput
 {
-    public long Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+  public long Id { get; set; }
+  public string Name { get; set; } = string.Empty;
+  public string Description { get; set; } = string.Empty;
 }
 
 public class ApplicationResult
 {
-    public long Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+  public long Id { get; set; }
+  public string Name { get; set; } = string.Empty;
+  public string Description { get; set; } = string.Empty;
 }
 
 public static class ApplicationInputMapperExtensions
 {
-    public static Application ToApplication(this ApplicationInput applicationInput) =>
-        new()
-        {
-            Id          = applicationInput.Id,
-            Name        = applicationInput.Name,
-            Description = applicationInput.Description,
-        };
+  public static Application ToApplication(this ApplicationInput applicationInput) =>
+      new()
+      {
+        Id = applicationInput.Id,
+        Name = applicationInput.Name,
+        Description = applicationInput.Description,
+      };
 
-    public static ApplicationResult ToApplicationResult(this Application application) =>
-        new()
-        {
-            Id          = application.Id,
-            Name        = application.Name,
-            Description = application.Description,
-        };
+  public static ApplicationResult ToApplicationResult(this Application application) =>
+      new()
+      {
+        Id = application.Id,
+        Name = application.Name,
+        Description = application.Description,
+      };
+
+  public static List<ApplicationResult> ToApplicationResults(this List<Application> applications)
+  {
+    if (applications.Count == 0)
+    {
+      return [];
+    }
+
+    return applications.Select(i => i.ToApplicationResult()).ToList();
+
+  }
 }
---
+++ Core/Application/GetAllApplicationsScenario.cs
@@ -0,0 +1,47 @@
+using EncyclopediaGalactica.Core.Infrastructure.Database;
+using EncyclopediaGalactica.Core.Operations.Scenarios;
+using LanguageExt;
+using Microsoft.EntityFrameworkCore;
+
+namespace EncyclopediaGalactica.Core.Application;
+
+public class GetAllApplicationsScenario(
+    DbContextOptions<DocumentDomainDbContext> dbContextOptions
+    )
+{
+
+  public Either<ErrorResult, List<ApplicationResult>> Execute(
+      GetAllApplicationsSagaContext context
+      )
+  {
+    Either<ErrorResult, List<ApplicationResult>> result =
+      from resultFromStorage in GetEntitiesFromStorage(context)
+      from mappedResult in MapResultsToContracts(resultFromStorage)
+      select mappedResult;
+    return result;
+  }
+
+  private static Either<ErrorResult, List<ApplicationResult>> MapResultsToContracts(
+      List<Application> resultFromStorage) =>
+    Either<ErrorResult, List<ApplicationResult>>.Right(resultFromStorage.ToApplicationResults());
+
+  private Either<ErrorResult, List<Application>> GetEntitiesFromStorage(
+      GetAllApplicationsSagaContext context
+      )
+  {
+    using DocumentDomainDbContext ctx = new(dbContextOptions);
+    try
+    {
+      List<Application> result = ctx.Applications.ToList();
+      return Either<ErrorResult, List<Application>>.Right(result);
+    }
+    catch (Exception e)
+    {
+      return Either<ErrorResult, List<Application>>.Left(
+          new ErrorResult(context.CorrelationId, e.Message)
+          );
+    }
+  }
+}
+
+public record GetAllApplicationsSagaContext(Guid CorrelationId);
